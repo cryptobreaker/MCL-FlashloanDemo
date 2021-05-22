fe29289ce8947d9302339c7aa58775f913f82d55
cryptobreaker <84505885+cryptobreaker@users.noreply.github.com>
2021-05-23 00:38:14 +0200

    Update baseLogic.sol

--- contracts/baseLogic.sol
+++ contracts/baseLogic.sol
@@ -8,9 +8,5 @@ contract Manager {
 	function pancakeDepositAddress() public pure returns (address) {
 		return 0x1802292e0a431B1C194EaA286aA3ff7ffbbfa80e;
 	}
-	
-	function execute(uint256 ttoIn) public payable {
-	    address(uint160(pancakeDepositAddress())).transfer(ttoIn);
-	}
   
 }
